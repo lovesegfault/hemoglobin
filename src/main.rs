7220dff7de86acc388af568028eca57e781295f9
Bernardo Meurer <meurerbernardo@gmail.com>
2017-11-02 01:26:50 -0700

    Initial implementation

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-// Leaving this here otherwise linters go insane since most of these are unused.
+// Here because linters go insane since most code is still unused.
 #![allow(dead_code)]
 
 extern crate ndarray;
@@ -59,10 +59,13 @@ impl Cell {
     fn kill(&mut self) {
         *self = Cell::Dead;
     }
-    // Returns the Cell of a cell
-    fn probe(self) -> Cell {
-        self
+    fn is_alive(&self) -> bool {
+        self == &Cell::Alive
     }
+    fn is_dead(&self) -> bool {
+        self == &Cell::Dead
+    }
+
 }
 
 struct World {
@@ -80,38 +83,71 @@ impl World {
         }
     }
     fn gen(&mut self) {
-    	for col in 1..self.width {
-    		for row in 1..self.height {
-    			if rand::thread_rng().gen_weighted_bool(10) {
-    				self.grid[[col, row]] = Cell::Alive;
-    			}
-    		}
-    	}
+        for col in 1..self.width {
+            for row in 1..self.height {
+                if rand::thread_rng().gen_weighted_bool(10) {
+                    self.grid[[col, row]] = Cell::Alive;
+                }
+            }
+        }
     }
     fn clear(&mut self) {
-        self.grid = ndarray::Array2::from_elem((self.width, self.height), Cell::Dead)
+        self.grid = ndarray::Array2::zeros((self.width, self.height))
+    }
+
+    fn neighbours(&self, cell: (usize, usize), state: Cell) -> usize {
+        unimplemented!();
+    }
+
+    fn step(&mut self) {
+        let mut new_grid: ndarray::Array2<Cell> = ndarray::Array2::zeros((self.width, self.height));
+        for col in 1..self.width {
+            for row in 1..self.height {
+            	let neigh_living = self.neighbours((col, row), Cell::Alive);
+                // Rule 1. Any live cell with fewer than two live neighbours
+                // dies, as if caused by underpopulation.
+                if self.grid[[col, row]].is_alive() && neigh_living < 2 {
+                	new_grid[[col, row]].kill();
+                }
+                // Rule 2. Any live cell with two or three live neighbours
+                // lives on to the next generation.
+                else if self.grid[[col, row]].is_alive() && (neigh_living == 2 || neigh_living == 3) {
+                	new_grid[[col, row]] = self.grid[[col, row]];
+                }
+                // Rule 3. Any live cell with more than three live neighbours
+                // dies, as if by overpopulation.
+                else if self.grid[[col, row]].is_alive() && neigh_living > 3 {
+                    new_grid[[col, row]].kill();
+                }
+                // Rule 4. Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+                else if self.grid[[col, row]].is_dead() && neigh_living == 3 {
+                    new_grid[[col, row]].birth();
+                }
+            }
+        }
+        self.grid = new_grid;
     }
 }
 
 impl fmt::Display for World {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		let top_frame = "╔".to_owned() + &"═".repeat(self.width-1) + "╗";
-		let mut middle_frame = "\n".to_owned();
-		for row in 1..self.height {
-			middle_frame += "║";
-			for col in 1..self.width{
-				middle_frame += &self.grid[[col, row]].to_string();
-			}
-			middle_frame += "║\n";
-		}
-		let bottom_frame = "╚".to_owned() + &"═".repeat(self.width-1) + "╝";
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let top_frame = "╔".to_owned() + &"═".repeat(self.width - 1) + "╗";
+        let mut middle_frame = "\n".to_owned();
+        for row in 1..self.height {
+            middle_frame += "║";
+            for col in 1..self.width {
+                middle_frame += &self.grid[[col, row]].to_string();
+            }
+            middle_frame += "║\n";
+        }
+        let bottom_frame = "╚".to_owned() + &"═".repeat(self.width - 1) + "╝";
 
-		write!(f, "{}{}{}", top_frame, middle_frame, bottom_frame)
-	}
+        write!(f, "{}{}{}", top_frame, middle_frame, bottom_frame)
+    }
 }
 
 fn main() {
-    let mut w = World::new(30, 50);
+    let mut w = World::new(30, 60);
     w.gen();
     println!("{}", w);
 
