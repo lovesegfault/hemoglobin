d6c8cef8d400446431072242675dc674712905b8
Bernardo Meurer <meurerbernardo@gmail.com>
2017-11-03 20:55:44 -0700

    Formatted with rustfmt

--- src/main.rs
+++ src/main.rs
@@ -30,7 +30,7 @@ impl World {
         }
     }
     fn gen(&mut self) {
-    	self.grid.clear();
+        self.grid.clear();
         for x in 1..self.width {
             for y in 1..self.height {
                 if rand::thread_rng().gen_weighted_bool(10) {
@@ -42,88 +42,85 @@ impl World {
 
     // This is an obviously dumb way to do this
     // TODO: Find a better way
-    fn neighbors(&self, cell: &(usize, usize)) -> Board{
-    	let mut neighbors: Board = HashSet::with_capacity(8);
-    	let (x, y) = (cell.0, cell.1);
+    fn neighbors(&self, cell: &(usize, usize)) -> Board {
+        let mut neighbors: Board = HashSet::with_capacity(8);
+        let (x, y) = (cell.0, cell.1);
 
-    	let top = y.checked_sub(1) != None;
-    	let bot = y.checked_add(1) <= Some(self.height);
-    	let right = x.checked_add(1) <= Some(self.width);
-    	let left = x.checked_sub(1) != None;
+        let top = y.checked_sub(1) != None;
+        let bot = y.checked_add(1) <= Some(self.height);
+        let right = x.checked_add(1) <= Some(self.width);
+        let left = x.checked_sub(1) != None;
 
-    	if right {
-    		neighbors.insert((x + 1, y));
-    	}
-    	if right && bot {
-    		neighbors.insert((x + 1, y + 1));
-    	}
-    	if right && top {
-    		neighbors.insert((x + 1, y - 1));
-    	}
-    	if bot {
-    		neighbors.insert((x, y + 1));
-    	}
-    	if top {
-    		neighbors.insert((x, y - 1));
-    	}
-    	if left {
-    		neighbors.insert((x - 1, y));
-    	}
-    	if left && bot {
-    		neighbors.insert((x - 1, y + 1));
-    	}
-    	if left && top {
-    		neighbors.insert((x - 1, y - 1));
-    	}
-    	neighbors
+        if right {
+            neighbors.insert((x + 1, y));
+        }
+        if right && bot {
+            neighbors.insert((x + 1, y + 1));
+        }
+        if right && top {
+            neighbors.insert((x + 1, y - 1));
+        }
+        if bot {
+            neighbors.insert((x, y + 1));
+        }
+        if top {
+            neighbors.insert((x, y - 1));
+        }
+        if left {
+            neighbors.insert((x - 1, y));
+        }
+        if left && bot {
+            neighbors.insert((x - 1, y + 1));
+        }
+        if left && top {
+            neighbors.insert((x - 1, y - 1));
+        }
+        neighbors
     }
 
     // TODO: Fix dumbness
-    fn neighbor_count(&self, cell: &(usize, usize)) ->
-    (Board, Board) {
-    	let mut neighbors: (Board, Board) =
-    		(HashSet::with_capacity(8), HashSet::with_capacity(8));
-    	for neighbor in self.neighbors(cell) {
-    		if self.grid.contains(&neighbor) {
-    			neighbors.0.insert(neighbor);
-    		} else {
-    			neighbors.1.insert(neighbor);
-    		}
-    	}
-    	neighbors
+    fn neighbor_count(&self, cell: &(usize, usize)) -> (Board, Board) {
+        let mut neighbors: (Board, Board) = (HashSet::with_capacity(8), HashSet::with_capacity(8));
+        for neighbor in self.neighbors(cell) {
+            if self.grid.contains(&neighbor) {
+                neighbors.0.insert(neighbor);
+            } else {
+                neighbors.1.insert(neighbor);
+            }
+        }
+        neighbors
     }
     // TODO: undumb
     fn step(&mut self) {
-    	let mut new_state: Board =
-    		HashSet::with_capacity(self.width * self.height);
+        let mut new_state: Board = HashSet::with_capacity(self.width * self.height);
 
-    	for cell in &self.grid {
-    		let (living, dead) = self.neighbor_count(cell);
-    		if living.len() < 2 || living.len() > 3 {}
-    		else if living.len() == 2 || living.len() == 3 {
-    			new_state.insert(*cell);
-    		}
+        for cell in &self.grid {
+            let (living, dead) = self.neighbor_count(cell);
+            if living.len() < 2 || living.len() > 3 {
+            } else if living.len() == 2 || living.len() == 3 {
+                new_state.insert(*cell);
+            }
 
-    		for neighbor in dead {
-    			if self.neighbor_count(cell).0.len() == 3 {
-    				new_state.insert(neighbor);
-    			}
-    		}
-    	}
-    	self.grid = new_state;
+            for neighbor in dead {
+                if self.neighbor_count(cell).0.len() == 3 {
+                    new_state.insert(neighbor);
+                }
+            }
+        }
+        self.grid = new_state;
     }
 
     fn render(&self, canvas: &mut Widget) {
-    	for x in 0..self.width {
-    		for y in 0..self.height {
-    			let mut cell = canvas.get_mut(x, y).unwrap();
-    			if self.grid.contains(&(x, y)) {
+        for x in 0..self.width {
+            for y in 0..self.height {
+                let mut cell = canvas.get_mut(x, y).unwrap();
+                if self.grid.contains(&(x, y)) {
                     cell.set_ch('\u{25AA}');
                 } else {
                     cell.set_ch(' ');
                 }
-    		}
-    	}
+            }
+        }
     }
 }
 
@@ -142,7 +139,8 @@ fn main() {
 
     'rendering: loop {
         while let Some(Event::Key(c)) =
-            term.get_event(Some(Duration::from_millis(0)).unwrap()).unwrap()
+            term.get_event(Some(Duration::from_millis(0)).unwrap())
+                .unwrap()
         {
             match c {
                 'q' => break 'rendering,
@@ -150,11 +148,11 @@ fn main() {
                 'n' => w.step(),
                 'a' => auto = true,
                 's' => auto = false,
-                _ => {},
+                _ => {}
             }
         }
         if auto {
-        	w.step();
+            w.step();
         }
         w.render(&mut canvas);
         canvas.draw_into(&mut term);
