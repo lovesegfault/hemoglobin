70adb3654458a0a988be83ebff4de932ccf79580
Daniel Sank <sank.daniel@gmail.com>
2017-11-05 02:15:03 -0800

    Add configurable rule.
    Fixes #6

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,14 @@
+extern crate bit_vec;
+extern crate num;
 extern crate rand;
 extern crate rustty;
 
-use std::collections::HashSet;
+use std::collections::{HashSet};
 use std::time::Duration;
+
+use bit_vec::BitVec;
+
+use num::bigint::BigInt;
 
 use rand::Rng;
 
@@ -15,14 +21,19 @@ type CellSet = HashSet<Cell>;
 struct World {
     height: usize,
     width: usize,
+    rule: BitVec,
     grid: CellSet,
 }
 
+
+
 impl World {
-    fn new((width, height): Cell) -> World {
+    fn new((width, height): Cell, rule: BigInt) -> World {
+        let (_, bytes) = rule.to_bytes_be();
         World {
             height: height,
             width: width,
+            rule: BitVec::from_bytes(&bytes),
             grid: HashSet::with_capacity(height * width),
         }
     }
@@ -35,6 +46,52 @@ impl World {
                 }
             }
         }
+    }
+
+    fn get_present_state(&self, cell: &Cell) -> usize {
+
+        let (x, y) = (cell.0, cell.1);
+        let top = y.checked_sub(1) != None;
+        let bot = y.checked_add(1) <= Some(self.height);
+        let right = x.checked_add(1) <= Some(self.width);
+        let left = x.checked_sub(1) != None;
+
+        let mut val = 0;
+
+        if left && top {
+            val += (self.grid.contains(&(x-1, y-1)) as usize) << 0;
+        }
+        if top {
+            val += (self.grid.contains(&(x, y-1)) as usize)  << 1;
+        }
+        if top && right {
+            val += (self.grid.contains(&(x+1, y-1)) as usize) << 2;
+        }
+        if left {
+            val += (self.grid.contains(&(x-1, y)) as usize) << 3;
+        }
+        val += (self.grid.contains(&(x, y)) as usize) << 4;
+        if right {
+            val += (self.grid.contains(&(x+1, y)) as usize) << 5;
+        }
+        if left && bot {
+            val += (self.grid.contains(&(x-1, y+1)) as usize) << 6;
+        }
+        if bot {
+            val += (self.grid.contains(&(x, y+1)) as usize) << 7;
+        }
+        if bot && right {
+            val += (self.grid.contains(&(x+1, y+1)) as usize) << 8;
+        }
+        val
+    }
+
+    fn make_dead_or_alive(&self, cell: &Cell) -> bool {
+        let state = self.get_present_state(cell);
+        if state > self.rule.len() -1 {
+            return false
+        }
+        return self.rule[state];
     }
 
     // This is an obviously dumb way to do this
@@ -75,7 +132,6 @@ impl World {
         neighbors
     }
 
-    // TODO: Fix dumbness
     fn neighbor_count(&self, cell: &Cell) -> (CellSet, CellSet) {
         let mut neighbors: (CellSet, CellSet) = (HashSet::with_capacity(8), HashSet::with_capacity(8));
         for neighbor in self.neighbors(cell) {
@@ -87,21 +143,19 @@ impl World {
         }
         neighbors
     }
-    // TODO: undumb
+
     fn step(&mut self) {
         let mut new_state: CellSet = HashSet::with_capacity(self.width * self.height);
 
         for cell in &self.grid {
             let (living, dead) = self.neighbor_count(cell);
-            if living.len() < 2 || living.len() > 3 {
-            } else if living.len() == 2 || living.len() == 3 {
-                new_state.insert(*cell);
-            }
-
-            for neighbor in dead {
-                if self.neighbor_count(cell).0.len() == 3 {
-                    new_state.insert(neighbor);
+            for itercell in living.union(&dead) {
+                if self.make_dead_or_alive(itercell) {
+                    new_state.insert(*itercell);
                 }
+            }
+            if self.make_dead_or_alive(cell) {
+                new_state.insert(*cell);
             }
         }
         self.grid = new_state;
@@ -123,24 +177,26 @@ impl World {
 
 
 fn main() {
+    let rule  = "98492".parse::<BigInt>().unwrap();
+
     //Create terminal and canvas
     let mut term = Terminal::new().unwrap();
     let mut canvas = Widget::new(term.size().0, term.size().1);
     canvas.align(&term, HorizontalAlign::Left, VerticalAlign::Top, 0);
 
     let (width, height) = canvas.size();
-    let mut w = World::new((width, height));
+    let mut w = World::new((width, height), rule);
     w.gen();
 
     let mut auto = false;
     let mut delay;
 
     'rendering: loop {
-    	if auto {
-    		delay = 0;
-    	} else {
-    		delay = 10;
-    	}
+        if auto {
+            delay = 0;
+        } else {
+            delay = 10;
+        }
         while let Some(Event::Key(c)) =
             term.get_event(Some(Duration::from_millis(delay)).unwrap())
                 .unwrap()
