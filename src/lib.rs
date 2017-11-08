36dca4b35425fa9e3f57b23d3ed42edbdf140bee
Daniel Sank <sank.daniel@gmail.com>
2017-11-08 02:59:20 -0800

    Add/improve tests.

--- src/lib.rs
+++ src/lib.rs
@@ -19,22 +19,28 @@ mod tests {
     use super::*;
     use num::PrimInt;
 
+    const EXPECTED_1082_BITS: [bool; 16] = [
+        false, true, false, true, false, false, false, false,
+        true, true, true, false, false, false, false, false];
+    // 1802 = 10 + 7*(2^8), so writing in little endian byte order but writing
+    // the bits within each byte with MSB on the left, we have
+    // [00001010][00000111].
+    // As a BitVec, we want 01010000 11100000.
+
     #[test]
     fn test_bitvec_bit_order() {
         // Consider a two-byte number where the firt byte's value is 10 and
         // the second byte's value is 7. Converting to a little endian byte
         // array should make the 0th byte 10 and the 1th byte 7.
-        let ten_seven = BigInt::from(10 + 7*(2.pow(8))); 
+        let ten_seven = BigInt::from(10 + 7*(2.pow(8)));
         let bytes = ten_seven.to_bytes_le().1;
         assert_eq!(bytes[0], 10);
         assert_eq!(bytes[1], 7);
-        // Now check that we can convert this to a BitVec. The bytes are in
-        // little endian order, but the bits within each byte are big endian:
+        // Now check what happens when we convert this to a BitVec. The bytes
+        // are in little endian order, but the bits within each byte are big
+        // endian:
         // [00001010][00000111]
         let bits = BitVec::from_bytes(&bytes);
-        for i in 0..16 {
-            println!("{}", bits[i]);
-        }
         let expected = vec![
             // 0th byte representing 10
             false,
@@ -60,13 +66,61 @@ mod tests {
     }
 
     #[test]
+    fn test_conway_kode() {
+        let expected = "476348294852520375132009738840824718882889556\
+                        423255282629108876378472743729817205343700177\
+                        683429960362194923168607044012736510546282236\
+                        08960".parse::<BigInt>().unwrap();
+        assert_eq!(expected, conway_kode());
+    }
+
+    #[test]
     fn test_grid_from_gidney_string() {
+
         let grid = grid_from_gidney_string(vec!["   ", "   "]);
-        assert_eq!(grid, CellSet::new());
-        let grid = grid_from_gidney_string(vec!["#  ", "   "]);
         let mut expected = CellSet::new();
+        assert_eq!(grid, expected);
+
+        let grid = grid_from_gidney_string(vec!["#  ", "   "]);
         expected.insert((0, 0));
         assert_eq!(grid, expected);
+
+        let grid = grid_from_gidney_string(vec!["#  ", " # "]);
+        expected.insert((1, 1));
+        assert_eq!(grid, expected);
+    }
+
+    #[test]
+    fn test_decimal_encoded_string_to_bitvec() {
+        let s = "1802";
+        let bitvec = decimal_encoded_string_to_bitvec(s);
+        for i in 0..16 {
+            assert_eq!(bitvec[i], EXPECTED_1082_BITS[i]);
+        }
+    }
+
+    #[test]
+    fn test_bigint_to_bitvec(){
+        let v = bigint_to_bitvec(BigInt::from(1802));
+        for i in 0..16 {
+            assert_eq!(v[i], EXPECTED_1082_BITS[i]);
+        }
+    }
+
+    #[test]
+    fn test_get_state() {
+        let mut grid = CellSet::new();
+        //  0
+        // 0#< look here
+        //  ^
+        grid.insert((0, 0));
+        assert_eq!(get_state(&grid, &(0, 0)), 16); // 2^4
+        //  01
+        // 0#-< look here
+        // 1-#
+        //  ^
+        grid.insert((1, 1));
+        assert_eq!(get_state(&grid, &(0, 0)), 272); // 2^4 + 2^8
     }
 }
 
@@ -100,9 +154,38 @@ pub fn grid_from_gidney_string(s: Vec<&str>) -> CellSet {
     result
 }
 
-fn rule_int_to_bitvec(x: BigInt) -> BitVec {
-    let padded_x = x + (BigInt::from(1) << 512);
-    BitVec::from_bytes(&padded_x.to_bytes_le().1)
+pub fn decimal_encoded_string_to_bitvec(s: &str) -> BitVec {
+    let val = s.parse::<BigInt>().unwrap();
+    bigint_to_bitvec(val)
+}
+
+fn bigint_to_bitvec(x: BigInt) -> BitVec {
+    let result_reversed = BitVec::from_bytes(&x.to_bytes_be().1);
+    let mut result = BitVec::from_elem(512, false);
+    for i in 0..result_reversed.len() {
+        result.set(
+            i,
+            result_reversed[result_reversed.len() - i - 1]);
+    }
+    result
+}
+
+fn get_state(grid: &CellSet, cell: &Cell) -> usize {
+    let (x, y) = (cell.0, cell.1);
+    let mut val = 0;
+    for dx in 0..3 {
+        for dy in 0..3 {
+            if match (x+dx).checked_sub(1) {
+                None => false,
+                Some(xx) => match (y+dy).checked_sub(1) {
+                    None => false,
+                    Some(yy) => grid.contains(&(xx, yy))
+                }
+            }{ val += 1 << (dx + (3*dy));
+            }
+        }
+    }
+    val
 }
 
 pub struct World {
@@ -113,17 +196,13 @@ pub struct World {
 }
 
 impl World {
-    pub fn new((width, height): Cell, rule: BigInt) -> World {
+    pub fn new((width, height): Cell, rule: BitVec) -> World {
         World {
             height: height,
             width: width,
-            rule: rule_int_to_bitvec(rule),
+            rule: rule,
             grid: HashSet::with_capacity(height * width),
         }
-    }
-
-    pub fn set_grid(&mut self, grid: CellSet) {
-        self.grid = grid;
     }
 
     pub fn gen(&mut self) {
@@ -137,32 +216,8 @@ impl World {
         }
     }
 
-    fn get_cell(&self, x: Option<usize>, y: Option<usize>) -> bool {
-        match x {
-            None => false,
-            Some(xx) => match y {
-                None => false,
-                Some(yy) => self.grid.contains(&(xx, yy))
-            }
-        }
-    }
-
-    fn get_state(&self, cell: &Cell) -> usize {
-
-        let (x, y) = (cell.0, cell.1);
-        let mut val = 0;
-        for dx in 0..3 {
-            for dy in 0..3 {
-                if self.get_cell((x+dx).checked_sub(1), (y+dy).checked_sub(1)) {
-                    val += 1 << (dx + (3*dy));
-                }
-            }
-        }
-        val
-    }
-
     fn decide_next_state(&self, cell: &Cell) -> bool {
-        let state = self.get_state(cell);
+        let state = get_state(&self.grid, cell);
         return self.rule[state];
     }
 
