7cd88b68fb856ad3a8f49f8512ffec529ed4d27e
Bernardo Meurer <meurerbernardo@gmail.com>
2018-12-12 05:20:57 -0800

    Remove dev tools from nix shell

--- shell.nix
+++ shell.nix
@@ -9,12 +9,6 @@ in
       # Rust
       nixpkgs.latest.rustChannels.stable.rust
       nixpkgs.latest.rustChannels.stable.cargo
-
-      # Dev tools
-      nixpkgs.neovim
-      nixpkgs.zsh
-      nixpkgs.git
-      nixpkgs.openssh
     ] ++ (if system == "x86_64-darwin" then [
       nixpkgs.darwin.apple_sdk.frameworks.Security
     ] else []);
